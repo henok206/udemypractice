da9424521d0f72f94d561787ffe1cf4c30cadaa2
Henok <henok_206@yahoo.com>
2022-06-23 11:19:33 +0300

    pro commite

--- script.js
+++ script.js
@@ -1,1 +1,103 @@
-alert("hello world");
+// Java script Fundamental one
+// about value
+// console.log("hello world");
+// console.log("ABEBE");
+// console.log(45);
+// // declear variable and assigned value
+// const firstName="Aster";
+// console.log(firstName);
+// // assignement about variable and value
+// const country="Ethiopia";
+// const continiet="Africa";
+// const population=120;
+// console.log(country);
+// console.log(continiet);
+// console.log(population);
+
+// about Data type
+//  const JavascriptIsFun=true;
+//  console.log(typeof JavascriptIsFun);
+//  console.log(typeof true);
+//  console.log(typeof JavascriptIsFun);
+//  console.log(typeof 34);
+//  console.log(typeof "Hello");
+ //Daynamic typing means varible can hold the new value
+//  JavascriptIsFun="kebede"
+//  console.log(typeof JavascriptIsFun);
+ //undefined data type
+//  const year;
+// //  console.log(year);
+//  console.log(typeof year);
+//  //daynamic typing if i assigned value for year 
+//  year=2022;
+//  console.log(typeof year);  
+
+// const ,const and var const see the different
+//  const age=30;
+//  age=31;//since age is muteble can be changed
+//  const birthyear=1978;
+//   birthyear=1898// birthdate can not be muitet
+//   const job; //const declaration must be intialized
+//  lastName="kebede";// without declearning
+//  console.log(lastName);
+
+// Basik operator
+//  const now=2022;
+// const ageAbe=now-1999;
+// const ageASter=now-2010;
+// console.log(ageAbe*2,ageASter/2);
+//concatination
+// const firstName="Abera";
+// const lastName="Kifile";
+// console.log(firstName +' ' + lastName);
+//Assignment operator
+// const x=10+5;//x=15
+// x+=10;//x=x+10=>15+10=25
+// x*=4//x=x*4=25*4=100
+// x++;//x=x+1=101
+// x--;//x=x-1=100
+// console.log(x);
+
+// //comparison operator
+// console.log(ageAbe>ageASter); //>,<,<=,>=
+//code challenge
+// const markWeight=78;
+// const markHeight=1.69;
+// const johnWeight=95;
+// const johnHeight=1.95;
+// const markWeight=95;
+// const markHeight=1.88;
+// const johnWeight=85;
+// const johnHeight=1.76;
+
+// const markbmi=markWeight/(markHeight*markHeight);
+// const johnbmi=johnWeight/(johnHeight*johnHeight);
+// console.log(markbmi,johnbmi);
+// const markHigherBmi= markbmi>johnbmi;
+// console.log(markbmi,johnbmi,markHigherBmi);
+
+// literal Template 
+// const firstName="Biniyam";
+// const job="teacher";
+// const birthyear=1999;
+// const year=2022;
+
+//  const Biniyam="i'm "+ firstName +',a '+(year-birthyear) + ' year old '+job+'!';
+//  console.log(Biniyam);
+// const newBiniyam=`I'm ${firstName},a ${year-birthyear}year old ${job}!`;
+// console.log(newBiniyam);
+// console.log(`Hello world!`);//we can use bacticks
+// //Multiple line string with\n and without \n just use 
+// //bacticks
+// console.log("abebe\n kebede\n Hiwot");//with \n
+// console.log(`Kebede
+// Aster
+// Girma`);
+
+
+
+
+
+
+
+
