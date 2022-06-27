c269a972c41991e38dfc156e4c4de91baea8be1e
Henok <henok_206@yahoo.com>
2022-06-27 17:52:59 +0300

    pro commit

--- script.js
+++ script.js
@@ -711,22 +711,83 @@
     // calculate temp amplitude
     //what amplitude: Answer max-min
     //
-     const temperatures=[3,-2,-6,-1,'error',9,13,17,15,14,9,5];
-    const calTemAmp=function(temps){
-      let max=temps[0];
-      let min=temps[0];
+     //const temperatures=[3,-2,-6,-1,'error',9,13,17,15,14,9,5];
+    // const calTemAmp=function(temps){
+    //   let max=temps[0];
+    //   let min=temps[0];
 
-       for(let i=0; i<temps.length; i++){
-          const curnTem =temps[i];
-          if( typeof curnTem !=='number') continue;
-          if(curnTem>max) max=curnTem;
-          if(curnTem<min) min=curnTem;
+    //    for(let i=0; i<temps.length; i++){
+    //       const curnTem =temps[i];
+    //       if( typeof curnTem !=='number') continue;
+    //       if(curnTem>max) max=curnTem;
+    //       if(curnTem<min) min=curnTem;
 
-       }
-       console.log(max,min);
-       return max-min
-    }
-    //calTemAmp([3,7,9,78]);
-     const amplitude=calTemAmp(temperatures);
-    console.log(amplitude);
+    //    }
+    //    console.log(max,min);
+    //    return max-min
+    // }
+    // //calTemAmp([3,7,9,78]);
+    //  const amplitude=calTemAmp(temperatures);
+    // console.log(amplitude);
+        
+        //Merge two array
+        // const calTemAmpNew=function(t1,t2){
+        //   const temps=t1.concat(t2);
+        //   console.log(temps);
+        //   let max=temps[0];
+        //   let min=temps[0];
+        //  for(let i=0; i<temps.length; i++){
+        //       const curnTem =temps[i];
+        //       if( typeof curnTem !=='number') continue;
+        //       if(curnTem>max) max=curnTem;
+        //       if(curnTem<min) min=curnTem;
     
+        //    }
+        //    console.log(max,min);
+        //    return max-min
+        // }
+        //calTemAmp([3,7,9,78]);
+        //  const amplitudeNew=calTemAmpNew([1,2,3],[4,5,6]);
+        // console.log(amplitudeNew);
+         
+        // how to fix BUG
+        //example
+        // const meseringKelvin=function(){
+        //    const meserment={
+        //       type:'temp',
+        //      unit:'celsius',
+        //      //FIXME by converting from string to num vidio 61
+        //      //value:Number(prompt(`Deegre celsius:`)),
+        //    }
+        //    const kelvin= meserment.value +273;
+        //    return kelvin;
+        // }
+
+        //  console.log(meseringKelvin());
+        /*Coding Challenge #1
+Given an array of forecasted maximum temperatures, the thermometer displays a
+string with the given temperatures. Example: [17, 21, 23] will print "... 17ºC in 1
+days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+Your tasks:
+1. Create a function 'printForecast' which takes in an array 'arr' and logs a
+string like the above to the console. Try it with both test datasets.
+2. Use the problem-solving framework: Understand the problem and break it up
+into sub-problems!
+Test data:
+§ Data 1: [17, 21, 23]
+§ Data 2: [12, 5, -5, 0, 4 */
+
+//  const data1= [17, 21, 23];
+//  const data2=[12, 5, -5, 0, 4];
+
+// const printForecast=function(arr){
+//      let str='';
+//    for(let i=0; i<arr.length; i++){
+//       str=str +`${arr[i]}c0 in ${i+1}days...`
+//        }
+//     console.log("..."+str);
+// }
+//  console.log(printForecast(data1));
+//  console.log(printForecast(data2));
+ 
+ 
